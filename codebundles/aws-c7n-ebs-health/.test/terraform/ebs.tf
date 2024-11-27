cfb684b8a19d53501548045dd0f0453b26e1c68a
saurabh3460 <saurabh3460@gmail.com>
2024-11-27 15:59:03 +0530

    remove volume check and add encrypted false in ebs.tf

--- codebundles/aws-c7n-ebs-health/.test/terraform/ebs.tf
+++ codebundles/aws-c7n-ebs-health/.test/terraform/ebs.tf
@@ -1,17 +1,8 @@
-# Check if the EBS volume exists (data source will fail if not found)
-data "aws_ebs_volumes" "existing_volumes" {
-  filter {
-    name   = "tag:Name"
-    values = [var.ebs_volume_name]
-  }
-}
-
 # Create the EBS volume if it doesn't exist
 resource "aws_ebs_volume" "ebs_volume" {
-  count             = length(data.aws_ebs_volumes.existing_volumes.ids) == 0 ? 1 : 0
   availability_zone = var.availability_zone
   size              = var.ebs_volume_size
-
+  encrypted = false
   tags = {
     Name = var.ebs_volume_name
   }
@@ -19,5 +10,5 @@ resource "aws_ebs_volume" "ebs_volume" {
 
 # Output
 output "volume_id" {
-  value = aws_ebs_volume.ebs_volume[0].id
+  value = aws_ebs_volume.ebs_volume.id
 }
