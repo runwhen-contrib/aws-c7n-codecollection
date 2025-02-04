6c458f15f289d1f3d2256a20f73efe5d2385878c
saurabh3460 <saurabh3460@gmail.com>
2025-02-04 16:26:00 +0530

    acm cb: Add ACM certificate resource and domain name variable for Terraform

--- codebundles/aws-c7n-acm-health/.test/terraform/main.tf
+++ codebundles/aws-c7n-acm-health/.test/terraform/main.tf
@@ -1,1 +1,23 @@
-# tf Placeholder
+# Create ACM Certificate
+resource "aws_acm_certificate" "cert" {
+  domain_name       = var.domain_name
+  validation_method = "DNS"
+
+  tags = {
+    Environment = "development"
+    Name        = "${var.domain_name}-certificate"
+  }
+
+  # Add www subdomain as subject alternative name
+  subject_alternative_names = ["www.${var.domain_name}"]
+}
+
+# Output the certificate ARN
+output "certificate_arn" {
+  value = aws_acm_certificate.cert.arn
+}
+
+# Output the DNS validation records
+output "validation_records" {
+  value = aws_acm_certificate.cert.domain_validation_options
+}
